adc38bc542a2836a0bfa2bc5784b0a39fd7b4cbf
Dacron <favouradebimpe63@gmail.com>
2022-08-21 02:16:06 +0100

    correctiong screen sizes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,95 +1,39 @@
-import React, { useState } from 'react'
+import React from 'react'
+import {motion} from 'framer-motion'
 import back from '/src/images/bg-card-back.png'
 import front from '/src/images/bg-card-front.png'
-import logo from '/src/images/card-logo.svg'
-import {motion} from 'framer-motion'
-import Details from './Details'
 
 function Card() {
-  const[cardNumber, setCardNumber] =useState(`0000 0000 0000 0000`)
-  const [cardName,setCardName] =useState('Jane Applessed')
-  const [month,setMonth] = useState('00')
-  const [year,setYear] = useState('00')
-  const [cvc, setCvc] = useState('000')
-
   return (
-   <>
-    <div className='container'>
-      
-       <div className="back-of-card">
-        <motion.img 
-        initial={{x:'300vw'}}
-        animate={{x:0}}
-        transition={{
-          delay:0.1,
-          type:'spring',
-          stiffness:30
-        }}
-        whileHover={{scale:1.1}}
-        src={back} alt="back of the card" />
-        <motion.p  
-      initial={{opacity:0}}
-      animate={{opacity:1}}
-      transition={{delay:1.9}}
-        className="CVC">
-          {cvc}
-          </motion.p>
-        </div>
+    <>
+      <div className="container">
 
+        <div className="back-of-card">
+           <img src={back} alt="" />
+           <p>000</p>
+        </div>
 
 
         <div className="front-of-card">
-        <motion.img 
-        initial={{x:'-300vw'}}
-       animate={{x:0}}
-       transition={{
-        delay:0.1,
-        type:'spring',
-        stiffness:30
-         }}
-          whileHover={{scale:1.1}}
-        className='information' src={front} alt="front of the card" />
-        <motion.img 
-         initial={{opacity:0}}
-         animate={{opacity:1}}
-         transition={{delay:1.6}}
-        className='logo' src={logo} alt="Card-Logo" />
-        </div>  
+          <img src={front} alt="" />
 
-        <motion.div 
-         initial={{opacity:0}}
-         animate={{opacity:1}}
-         transition={{delay:1.6}}
-        className="Card-Details">
+         <div className="cardBody">
 
-          <div className="card-number">
-          <h4>{cardNumber}</h4>
-          </div>
-        
-        <div className="Card-bottom">
-        <p>{cardName}</p>
-        <div className="dates">
-      <p className="month"> {month}  </p>
-      <p className='year'>/{year} </p>
-     </div>
-        
+         <h3>0000 0000 0000 0000</h3>
+
+         <div className="bottomBody">
+            <p>Jane Appleased</p>
+             <p>00/00</p>
+         </div>
+
+         </div>
+
         </div>
-     
-    
-        </motion.div>
+        
 
 
-    
-    </div>
-    <Details 
-    setCardNumber={setCardNumber}
-    setCardName={setCardName}
-    setMonth={setMonth}
-    setYear={setYear}
-    setCvc={setCvc}
-    /> 
-   </>
-    
+      </div>
+    </>
   )
 }
 
